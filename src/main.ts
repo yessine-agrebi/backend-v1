1fb5ffa5aa0b6d1d01dc663765ed0ff160880d80
yessine-agrebi <yessine.agrebi.aiesec@gmail.com>
2024-02-23 15:55:38 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({"origin":"localhost:3000"});
   await app.listen(8000);
 }
 bootstrap();
