2b2c088c7bc0451ee8dd259c043ad6a310c27ce7
yessine-agrebi <yessine.agrebi.aiesec@gmail.com>
2024-02-25 21:54:05 +0100

    set global prefix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: 'localhost:3000' });
+  app.setGlobalPrefix('api/v1');
   await app.listen(8000);
 }
 bootstrap();
