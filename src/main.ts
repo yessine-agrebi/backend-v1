09bd4e81ce8f91dad1cda33652ae30a88bbd6f2f
yessine-agrebi <yessine.agrebi.aiesec@gmail.com>
2024-02-26 13:15:54 +0100

    fix cors and add users controller

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'localhost:3000' });
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   app.setGlobalPrefix('api/v1');
   await app.listen(8000);
 }
