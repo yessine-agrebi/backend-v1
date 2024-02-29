e6ffd91f316d3fbe337cb596ee65d2b7a74e2acd
yessine-agrebi <yessine.agrebi.aiesec@gmail.com>
2024-02-29 09:36:20 +0100

    fix cors issue

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: 'http://localhost:3000',
-    credentials: true,
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Authorization', 'Accept', 'X-Access-Token'],
+      origin: 'http://localhost:3000',
+      credentials: true,
+      methods: ['POST', 'GET', 'PATCH', 'DELETE', 'OPTIONS']
+    }
   });
+  
   app.setGlobalPrefix('api/v1');
   await app.listen(8000);
 }
